86b7fc3266c631227f5c7bfbb55389ff8376ca50
Frederic <yassifrederic@gmail.com>
2023-01-14 21:41:38 +0100

    debut conteneurisation

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+#
+# Build stage
+#
+FROM maven:3.8.3-openjdk-17 AS build
+COPY src /home/app/src
+COPY pom.xml /home/app
+RUN mvn -f /home/app/pom.xml clean package
+
+#
+# Package stage
+#
+FROM openjdk:17
+EXPOSE 8080
+ADD target/backend-0.0.1-SNAPSHOT.jar app.jar 
+COPY --from=build /home/app/target/backend-0.0.1-SNAPSHOT.jar /usr/local/lib/app.jar
+ENTRYPOINT ["java","-jar","/usr/local/lib/app.jar"]
+
+#ENTRYPOINT ["java","-jar","app.jar","--spring.profiles.active=nosecurity"]
--- src/main/java/com/backend/ProjetMioTSpringApplication.java
+++ src/main/java/com/backend/ProjetMioTSpringApplication.java
@@ -1,5 +1,7 @@
 package com.backend;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,9 @@ public class ProjetMioTSpringApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ProjetMioTSpringApplication.class, args);
+		//SpringApplication.run(ProjetMioTSpringApplication.class, args);
+		SpringApplication app = new SpringApplication(ProjetMioTSpringApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
+		app.run(args);
 	}
 }
